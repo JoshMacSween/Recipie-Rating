497f53f2ffedd66da0e0bd5083afcc67e9a1fd1e
Joshua MacSween <joshmacsween@gmail.com>
2020-09-07 13:57:48 -0600

    working star rating

--- src/Color.js
+++ src/Color.js
@@ -8,6 +8,7 @@ export default function Color({
   color,
   rating,
   onRemove = (f) => f,
+  onRate = (f) => f,
 }) {
   return (
     <section>
@@ -16,7 +17,9 @@ export default function Color({
         <FaTrash />
       </button>
       <div style={{ height: 50, backgroundColor: color }} />
-      <StarRating selectedStars={rating} />
+      <StarRating selectedStars={rating}
+        onRate={rating => onRate(id, rating)}
+      />
     </section>
   )
 }
--- src/StarRating.js
+++ src/StarRating.js
@@ -7,14 +7,18 @@ const Star = ({ selected = false, onSelect = (f) => f }) => (
 
 const createArray = (length) => [...Array(length)]
 
-export default function StarRating({ totalStars = 5, selectedStars = 0 })
-{
+export default function StarRating({
+  totalStars = 5,
+  selectedStars = 0,
+  onRate = (f) => f,
+}) {
   return (
     <>
       {createArray(totalStars).map((n, i) => (
         <Star
           key={i}
           selected={selectedStars > i}
+          onSelect={() => onRate(i + 1)}
         />
       ))}
       <p>
